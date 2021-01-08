8778ec1474926d2a12ab4ba8b3565a4a9efb9a00
Sanchit Garge <garg1999san@gmail.com>
2021-01-08 20:14:26 +0530

    Final Webpage

--- app.js
+++ app.js
@@ -51,8 +51,8 @@ passport.deserializeUser(function(id, done) {
 });
 
 passport.use(new GoogleStrategy({
-    clientID: "1078571631534-kmcpv7tt1c3keiv1ktiakd99v88vri2c.apps.googleusercontent.com",
-    clientSecret: "3vSOFb1fZjnX2zc0MTxnn5_r",
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
     callbackURL: "https://gentle-peak-11148.herokuapp.com/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
